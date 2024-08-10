e6245910fd156ae3b876e620b48ddffda98f8bea
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-08-10 06:03:02 -0400

    Loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Settings from "./pages/Settings";
-import Profile from "./pages/Profile";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Profile = lazy(() => import("./pages/Profile"));
 
 function App() {
     return (
         <>
             <Router>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/messages" element={<Chat />} />
-                    <Route path="/settings" element={<Settings />} />
-                    <Route path="/profile" element={<Profile />} />
-                </Routes>
+                <Suspense fallback={<Loader />}>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/messages" element={<Chat />} />
+                        <Route path="/settings" element={<Settings />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </Routes>
+                </Suspense>
             </Router>
         </>
     );
