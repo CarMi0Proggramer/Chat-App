ab231b76bd901b2aef398bd03411203ba1eeebc2
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-08-17 07:23:10 -0400

    Kevin's pr checked completely

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,212 +1,97 @@
 import { Avatar } from "flowbite-react";
 
+type MessageParams = {
+    name: string;
+    messagePreview: string;
+    lastTime: string;
+    url: string;
+    unReadMessages: number;
+};
+function Message({
+    name,
+    messagePreview,
+    lastTime,
+    url,
+    unReadMessages,
+}: MessageParams) {
+    return (
+        <article className="flex h-16 items-center px-2 py-1 gap-3 border-b border-b-gray-400 dark:border-b-gray-700]">
+            <div>
+                <Avatar img={url} />
+            </div>
+            <div className="flex-1 flex truncate justify-between">
+                <div className="flex flex-col max-w-[80%] lg:max-w-[90%]">
+                    <h3 className="text-lg font-semibold">{name}</h3>
+                    <p className="truncate dark:text-gray-400">
+                        {messagePreview}
+                    </p>
+                </div>
+                <div className="flex flex-col">
+                    <span>{lastTime}</span>
+                    <span className="grid place-content-center rounded-full bg-green-300 dark:bg-green-600">
+                        {unReadMessages}
+                    </span>
+                </div>
+            </div>
+        </article>
+    );
+}
+
 export default function Chat() {
     return (
-        <div className="flex flex-col w-full flex-1 overflow-y-auto my-[65px]">
-            <div className="flex justify-evenly  items-center h-12 ">
-                <button className="flex rounded text-white justify-center items-center bg-green-700   h-6 w-max px-2 ">
-                    Todos
-                </button>
-                <button className="flex rounded text-white justify-center items-center bg-green-700   h-6 w-max px-2">
-                    No leidos
-                </button>
-                <button className="flex rounded text-white justify-center items-center bg-green-700   h-6 w-max px-2">
-                    Grupos
-                </button>
-            </div>
-            <div className=" dark:text-gray-300 w-full flex-col  ">
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la
-                            tarASSASeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black   items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            Kevin Da Silva
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border  border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            foo
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-                <div className="flex w-full h-16 border border-black  items-center">
-                    <div className="mr-4 ml-1">
-                        <Avatar img="" />
-                    </div>
-                    <div className="flex flex-col  h-3/4 overflow-hidden w-[100%] items-start justify-center">
-                        <h4 className=" text-ls font-semibold">
-                            ghgh
-                        </h4>
-                        <p className=" text-sm h-max w-max ">
-                            Ya hiciste la tarASSASeaaaaa?
-                        </p>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-sm">12:12</div>
-                        <div className="bg-green-700 text-sm rounded-full h-max w-max px-1">
-                            10
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <main className="flex flex-col w-full flex-1 overflow-y-auto my-[65px]">
+            <section>
+                <nav>
+                    <ul className="w-full flex justify-evenly items-center text-white h-12 text-lg bg-green-300 dark:bg-green-600">
+                        <li className="">
+                            <a href="#">All</a>
+                        </li>
+                        <li className="">
+                            <a href="#">Unread</a>
+                        </li>
+                        <li className="">
+                            <a href="#">Groups</a>
+                        </li>
+                    </ul>
+                </nav>
+            </section>
+            <section className="dark:text-gray-300 w-full flex flex-col gap-2  ">
+                <Message
+                    name="Kevin Da Silva"
+                    messagePreview="Ya completaste las tareas acuerdate que tienes que estudiar historia o si no no vas a poder ir a la universidad porque para programar hace falta que sepas historia."
+                    lastTime="5:12pm"
+                    url=""
+                    unReadMessages={3}
+                />
+                <Message
+                    name="Kevin Da Silva"
+                    messagePreview="Ya completaste las tareas acuerdate que tienes que estudiar historia o si no no vas a poder ir a la universidad porque para programar hace falta que sepas historia."
+                    lastTime="5:12pm"
+                    url=""
+                    unReadMessages={3}
+                />
+                <Message
+                    name="Kevin Da Silva"
+                    messagePreview="Ya completaste las tareas acuerdate que tienes que estudiar historia o si no no vas a poder ir a la universidad porque para programar hace falta que sepas historia."
+                    lastTime="5:12pm"
+                    url=""
+                    unReadMessages={3}
+                />
+                <Message
+                    name="Kevin Da Silva"
+                    messagePreview="Ya completaste las tareas acuerdate que tienes que estudiar historia o si no no vas a poder ir a la universidad porque para programar hace falta que sepas historia."
+                    lastTime="5:12pm"
+                    url=""
+                    unReadMessages={3}
+                />
+                <Message
+                    name="Kevin Da Silva"
+                    messagePreview="Ya completaste las tareas acuerdate que tienes que estudiar historia o si no no vas a poder ir a la universidad porque para programar hace falta que sepas historia."
+                    lastTime="5:12pm"
+                    url=""
+                    unReadMessages={3}
+                />
+            </section>
+        </main>
     );
 }
