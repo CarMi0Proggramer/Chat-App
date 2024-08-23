859b0db14bb6ac26c194bc0b20d8d100798c3bcb
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-08-23 14:41:05 -0400

    Chat improved

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,7 +5,7 @@ type MessageParams = {
     messagePreview: string;
     lastTime: string;
     url: string;
-    unReadMessages: number;
+    unReadMessages?: number;
 };
 function Message({
     name,
@@ -15,39 +15,41 @@ function Message({
     unReadMessages,
 }: MessageParams) {
     return (
-        <article className="flex h-16 items-center px-2 py-1 gap-3 border-b border-b-gray-400 dark:border-b-gray-700]">
+        <a
+            href="#"
+            className="flex h-16 items-center px-2 py-1 gap-3 border-b border-b-gray-400 dark:border-b-gray-700 hover:bg-gray-200 dark:hover:bg-gray-800"
+        >
             <div>
                 <Avatar img={url} />
             </div>
             <div className="flex-1 flex truncate justify-between">
                 <div className="flex flex-col max-w-[80%] lg:max-w-[90%]">
-                    <h3 className="text-base md:text-lg font-semibold">
+                    <h3 className="text-base md:text-lg font-medium dark:text-white">
                         {name}
                     </h3>
-                    <p className="text-sm md:text-base truncate dark:text-gray-400">
+                    <p className="text-sm md:text-base truncate text-gray-800 dark:text-gray-400">
                         {messagePreview}
                     </p>
                 </div>
-                <div className="flex flex-col text-sm md:text-base">
+                <div className="grid place-content-center text-sm md:text-base">
                     <span>{lastTime}</span>
                     <span className="grid place-content-center rounded-full bg-green-300 dark:bg-green-600">
                         {unReadMessages}
                     </span>
                 </div>
             </div>
-        </article>
+        </a>
     );
 }
 
 export default function Chat() {
     return (
-        <section className="dark:text-gray-300 w-full flex flex-col gap-2  ">
+        <section className="dark:text-gray-300 w-full flex flex-col">
             <Message
                 name="Kevin Da Silva"
                 messagePreview="Ya completaste las tareas acuerdate que tienes que estudiar historia o si no no vas a poder ir a la universidad porque para programar hace falta que sepas historia."
                 lastTime="5:12pm"
                 url=""
-                unReadMessages={3}
             />
             <Message
                 name="Kevin Da Silva"
--- src/pages/Chats.tsx
+++ src/pages/Chats.tsx
@@ -6,17 +6,17 @@ export default function Chats() {
     return (
         <Layout>
             <main className="flex flex-col w-full flex-1 overflow-y-auto my-[65px]">
-                <section>
+                <section className="p-1 bg-green-300 dark:bg-green-600">
                     <nav>
-                        <ul className="w-full flex justify-evenly items-center text-white h-12 bg-green-300 dark:bg-green-600 font-medium text-lg md:text-xl">
-                            <li className="">
+                        <ul className="w-full flex justify-evenly items-center text-white h-12 font-medium text-base md:text-lg">
+                            <li className="hover:underline">
                                 <a href="#">View All</a>
                             </li>
-                            <li className="">
+                            <li className="hover:underline">
                                 <a href="#">Unread</a>
                             </li>
-                            <li className="">
-                                <a href="#">Groups</a>
+                            <li className="hover:underline">
+                                <a href="#">Archived</a>
                             </li>
                         </ul>
                     </nav>
